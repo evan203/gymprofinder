640eed37d5f326e15efb341f7f7a30c2501f0246
evan203 <evancreeper203@gmail.com>
2023-10-07 12:17:18 -0500

    profile route,  api stuff

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,12 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
-
+import { profileRouter } from "./routers/profile";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
