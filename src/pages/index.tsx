0df235d9541875066cc4ffc62cc47773ebf9f4a4
evan203 <evancreeper203@gmail.com>
2023-10-18 11:43:24 -0500

    redesign user list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,33 @@ import { InfiniteUserList } from "~/components/InfiniteUserList";
 import { LoggedOutLanding } from "~/components/LoggedOutLanding";
 import { api } from "~/utils/api";
 
-function Users() {
+function LoggedInHome() {
   const users = api.profile.infiniteFeed.useInfiniteQuery( 
     {},
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
   return (
-    <InfiniteUserList 
-      users={users.data?.pages.flatMap((page) => page.users)}
-      isError={users.isError}
-      isLoading={users.isLoading}
-      hasMore={false}
-      fetchNewUsers={users.fetchNextPage}
-    />
+    <>
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#870000] to-[#250000]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Gym <span className="text-[#ff3333]">Pro</span> Finder
+          </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+              <InfiniteUserList 
+                users={users.data?.pages.flatMap((page) => page.users)}
+                isError={users.isError}
+                isLoading={users.isLoading}
+                hasMore={false}
+                fetchNewUsers={users.fetchNextPage}
+              />
+          </div>
+        </div>
+      </main>
+
+
+
+    </>
   )
 }
 export default function Home() {
@@ -29,7 +43,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        {session.status === "authenticated" ? <Users /> : <LoggedOutLanding /> }
+        {session.status === "authenticated" ? <LoggedInHome /> : <LoggedOutLanding /> }
       </div>
     </>
   );
