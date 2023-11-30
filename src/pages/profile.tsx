2da645ec7264283022978fb2eee9ef8807f98643
evan203 <evancreeper203@gmail.com>
2023-11-29 21:20:50 -0600

    iframe :(

---
+++ src/pages/profile.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>GymProFinder</title>
+        <meta name="description" content="GymProFinder" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <iframe src="https://sites.google.com/stu.naperville203.org/gymbrofinder/search-for-other-gymbros/paper-profile?authuser=0"></iframe>
+    </>
+  );
+}
